3487833f27f1473c171006ffed1877a7402a4d5c
Nazareno Altamiranda <nazalt256@gmail.com>
2022-02-10 16:48:17 -0300

    [FIXING] Crew Slider now light the selected item

--- src/molecules/PointNav/index.js
+++ src/molecules/PointNav/index.js
@@ -1,14 +1,42 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 // styles
 import { Wrapper } from './PointNav';
 
 const PointNav = ({ onClick }) => {
+
+  const addActiveClass = (e) => {
+    const listDivs = document.querySelectorAll('.point');
+    listDivs.forEach(div => {
+      if(div.classList.contains("active")) {
+        div.classList.remove("active");
+      }
+
+     e.currentTarget.classList.add("active")
+    })
+      
+    console.log(listDivs)
+  }
+
   return (
     <Wrapper>
-      <div className="point" onClick={(e) => onClick(e)} value="0"></div>
-      <div className="point" onClick={(e) => onClick(e)} value="1"></div>
-      <div className="point" onClick={(e) => onClick(e)} value="2"></div>
-      <div className="point" onClick={(e) => onClick(e)} value="3"></div>
+      <div 
+        className="point"
+        onClick={(e) => {onClick(e); addActiveClass(e) }}
+        value="0"
+      ></div>
+      <div 
+        className="point"
+        onClick={(e) => {onClick(e); addActiveClass(e) }}
+        value="1"
+      ></div><div 
+        className="point"
+        onClick={(e) => {onClick(e); addActiveClass(e) }}
+        value="2"
+      ></div><div 
+        className="point"
+        onClick={(e) => {onClick(e); addActiveClass(e) }}
+        value="3"
+      ></div>
     </Wrapper>
   )
 }
