cd37a8bc4e6448dfab755937954e1a5028d3af2b
Nazareno Altamiranda <nazalt256@gmail.com>
2022-02-12 03:21:17 -0300

    [FIXING] MainButton now redirect to '/destinations'

--- src/molecules/MainButton/index.js
+++ src/molecules/MainButton/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom'
 // styles
 import { Wrapper, Button, HoverShadow } from './MainButton.css';
 // molecules
@@ -6,7 +7,9 @@ const MainButton = () => {
   return (
     <Wrapper>
       <HoverShadow />
-      <Button><span>Explore</span></Button>
+      <Link to="/destination">
+        <Button><span>Explore</span></Button>
+      </Link>
     </Wrapper>
   );
 };
